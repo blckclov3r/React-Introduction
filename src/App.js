29c04acd7bf2fe7652575334631031d8379f4c91
aljunajio <blckclov3r@gmail.com>
2022-08-10 11:43:31 +0800

    Revert "Update Changes"
    This reverts commit 416ca1bded8fc5fed7c6244b8e51d335d72ea11e.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Game from './components/Game'
 import './App.css'
 import Header from './components/Header'
@@ -6,22 +6,10 @@ import { BrowserRouter,Routes,Route} from "react-router-dom";
 import Game2 from './pages/Game2';
 import Rules from './pages/Rules';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer,toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import Footer from './components/Footer';
 export default function App() {
   
-  useEffect(()=>{
-    toast.info('ようこそゲストさん！', {
-      position: "top-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      });
-  },[])
-
   return (
     <div className='container mainapp'>
     
